aa94ffed5981f6f891f04ef5a04155f03f9e1910
Calin Peters <calinpeters98@hotmail.nl>
2022-11-04 12:51:22 +0100

    Implemented GetAll query in Application layer

---
+++ src/ForumsService.Application/Queries/GetAllForums/GetAllForumsHandler.cs
@@ -0,0 +1,22 @@
+﻿using ForumsService.Application.Interfaces.Repositories;
+using ForumsService.Domain.Entities;
+using MediatR;
+
+namespace ForumsService.Application.Queries.GetAllForums
+{
+    public class GetAllForumsHandler : IRequestHandler<GetAllForumsQuery, IEnumerable<ForumDto>>
+    {
+        private readonly IQueryForumRepository _repository;
+
+        public GetAllForumsHandler(IQueryForumRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<ForumDto>> Handle(GetAllForumsQuery request, CancellationToken cancellationToken)
+        {
+            var forumList = await _repository.GetAll();
+            return forumList;
+        }
+    }
+}
