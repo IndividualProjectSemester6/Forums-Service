d8e88056ed71533a9deb14816923296de845f3e7
Calin Peters <calinpeters98@hotmail.nl>
2022-11-04 12:19:56 +0100

    Domain project now in proper folder location

---
+++ src/ForumsService.Domain/Entities/ForumDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ForumsService.Domain.Entities
+{
+    public class ForumDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string[] Rules { get; set; }
+
+        public ForumDto(Guid id, string name, string description, string[] rules)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Rules = rules;
+        }
+    }
+}
