60dfabb6d59eec5d4c582c7896c0e038d97b33c7
Calin Peters <calinpeters98@hotmail.nl>
2022-11-04 13:00:10 +0100

    Added implementation for controller

---
+++ src/ForumsService.API/Controllers/ForumsController.cs
@@ -0,0 +1,34 @@
+﻿using ForumsService.API.Models;
+using ForumsService.Application.Queries.GetAllForums;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumsService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ForumsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ForumsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ForumViewModel>> GetAll()
+        {
+            var query = new GetAllForumsQuery();
+            var result = await _mediator.Send(query);
+            List<ForumViewModel> forums = new List<ForumViewModel>();
+            foreach (var forum in result)
+            {
+                forums.Add(new ForumViewModel(forum.Id, forum.Name, forum.Description, forum.Rules));
+            }
+
+            return Ok(forums);
+        }
+    }
+}
---
+++ src/ForumsService.API/Models/ForumViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ForumsService.API.Models
+{
+    public class ForumViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string[] Rules { get; set; }
+
+        public ForumViewModel(Guid id, string name, string description, string[] rules)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Rules = rules;
+        }
+
+        public ForumViewModel()
+        {
+        }
+    }
+}
