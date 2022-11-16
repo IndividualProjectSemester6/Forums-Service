ed471305f33254bc0b50c505b5b43faeaee688b6
Calin Peters <calinpeters98@hotmail.nl>
2022-11-16 09:43:15 +0100

    Added implementation for command repository

--- src/ForumsService.Application/Interfaces/Repositories/ICommandForumRepository.cs
+++ src/ForumsService.Application/Interfaces/Repositories/ICommandForumRepository.cs
@@ -5,7 +5,7 @@ namespace ForumsService.Application.Interfaces.Repositories
     public interface ICommandForumRepository
     {
         Task<ForumDto> CreateForum(ForumDto forum);
-        Task<ForumDto?> DeleteForum(ForumDto forum);
+        Task<ForumDto?> DeleteForum(Guid id);
         Task<ForumDto?> UpdateForum(ForumDto forum);
     }
 }
