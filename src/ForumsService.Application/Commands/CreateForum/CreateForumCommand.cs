0cc863de7a9048c27450cd057cf27126dcb38298
Calin Peters <calinpeters98@hotmail.nl>
2022-11-16 09:49:36 +0100

    Added logic impl of create forum

---
+++ src/ForumsService.Application/Commands/CreateForum/CreateForumCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using ForumsService.Domain.Entities;
+using MediatR;
+
+namespace ForumsService.Application.Commands.CreateForum
+{
+    public class CreateForumCommand : IRequest<ForumDto>
+    {
+        public string Name { get; }
+        public string Description { get; }
+
+        [JsonConstructor]
+        public CreateForumCommand(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ src/ForumsService.Application/Commands/CreateForum/CreateForumHandler.cs
@@ -0,0 +1,23 @@
+﻿using ForumsService.Application.Interfaces.Repositories;
+using ForumsService.Domain.Entities;
+using MediatR;
+
+namespace ForumsService.Application.Commands.CreateForum
+{
+    public class CreateForumHandler : IRequestHandler<CreateForumCommand, ForumDto>
+    {
+        private readonly ICommandForumRepository _repository;
+
+        public CreateForumHandler(ICommandForumRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ForumDto> Handle(CreateForumCommand request, CancellationToken cancellationToken)
+        {
+            var forum = new ForumDto(Guid.NewGuid(), request.Name, request.Description);
+            var result = await _repository.CreateForum(forum);
+            return result;
+        }
+    }
+}
